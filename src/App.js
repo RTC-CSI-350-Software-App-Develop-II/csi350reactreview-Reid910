647234320b638f451a3f3dbca8e0d05fc917b2dc
Sam Nelson <samn10.nelson@gmail.com>
2025-05-25 19:22:16 -0700

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+
+import RestaurantsContainer from './components/RestaurantsContainer';
 import "./App.css";
+
+import {data} from './data/data';
 
 //App is the root of our application and where we load in our components.
 function App() {
-
-
   return (
     <div className="App">
+      <RestaurantsContainer restaurants={data}/>
     </div>
   );
 }
